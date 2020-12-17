23570d33509959a09b42ec59d81e4ce78b3a72fa
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2020-12-17 11:04:52 +0200

    Implement splash screen

--- app/src/main/java/com/verygoodsecurity/veryspacyfood/MainActivity.kt
+++ app/src/main/java/com/verygoodsecurity/veryspacyfood/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.verygoodsecurity.veryspacyfood
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_App)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
