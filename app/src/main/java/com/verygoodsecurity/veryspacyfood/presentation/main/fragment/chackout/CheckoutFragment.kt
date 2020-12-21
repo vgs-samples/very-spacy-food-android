5fbaf3adbb7118e0efe7f0f4c2a3765faca2d7e4
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2020-12-21 16:25:38 +0200

    Implement checkout UI

--- app/src/main/java/com/verygoodsecurity/veryspacyfood/presentation/main/fragment/chackout/CheckoutFragment.kt
+++ app/src/main/java/com/verygoodsecurity/veryspacyfood/presentation/main/fragment/chackout/CheckoutFragment.kt
@@ -5,14 +5,34 @@ import androidx.fragment.app.activityViewModels
 import com.verygoodsecurity.veryspacyfood.R
 import com.verygoodsecurity.veryspacyfood.presentation.main.fragment.core.BaseMainFragment
 import com.verygoodsecurity.veryspacyfood.presentation.main.viewmodel.MainViewModel
+import com.verygoodsecurity.veryspacyfood.util.extension.hide
+import com.verygoodsecurity.veryspacyfood.util.extension.show
 import com.verygoodsecurity.veryspacyfood.util.extension.showShort
+import kotlinx.android.synthetic.main.main_toolbar.*
 
 class CheckoutFragment : BaseMainFragment(R.layout.fragment_checkout) {
 
     private val viewModel: MainViewModel by activityViewModels()
 
     override fun initView(savedInstanceState: Bundle?) {
+        initToolbar()
+        initContent()
+        initListeners()
+    }
+
+    private fun initToolbar() {
+        mtvMainToolbarBack?.show()
+        ivMainToolbarBasket?.hide()
+    }
+
+    private fun initContent() {
         requireContext().showShort(viewModel.paymentCardLiveData.value.toString())
+    }
+
+    private fun initListeners() {
+        mtvMainToolbarBack?.setOnClickListener {
+            navigation.navigateBack()
+        }
     }
 
     companion object {
