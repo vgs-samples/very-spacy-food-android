73c53ec8dbead5fcb5ab4d50a93c123f7dd245dc
dmytrodmytryshyn <dmytro.dmytryshyn@vgs.io>
2021-11-03 17:40:49 +0200

    Fix lint check

--- app/src/main/java/com/verygoodsecurity/veryspacyfood/data/dto/Transaction.kt
+++ app/src/main/java/com/verygoodsecurity/veryspacyfood/data/dto/Transaction.kt
@@ -2,6 +2,7 @@ package com.verygoodsecurity.veryspacyfood.data.dto
 
 import com.google.gson.annotations.SerializedName
 
+@Suppress("unused")
 class Transaction constructor(
     @SerializedName("userId") private val userId: String,
     @SerializedName("amount") private val amount: Int
